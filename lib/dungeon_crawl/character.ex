b9d0cd9886f6fb603f678bb01cd943142afadc69
Rowa <jrowah@gmail.com>
2023-03-03 18:46:35 +0300

    creats a char struct with attributes

---
+++ lib/dungeon_crawl/character.ex
@@ -0,0 +1,8 @@
+defmodule DungeonCrawl.Character do
+  defstruct name: nil,
+            destruction: nil,
+            hit_points: 0,
+            max_hit_points: 0,
+            attack_description: nil,
+            damage_range: nil
+end
