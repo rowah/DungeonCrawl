7d6637ab798a28301c8cf4f8b09521ef8dd84601
Rowa <jrowah@gmail.com>
2023-03-05 17:20:57 +0300

    creats list of heroes using the DungeonCrawl.Character struct

--- lib/dungeon_crawl/character.ex
+++ lib/dungeon_crawl/character.ex
@@ -1,6 +1,6 @@
 defmodule DungeonCrawl.Character do
   defstruct name: nil,
-            destruction: nil,
+            description: nil,
             hit_points: 0,
             max_hit_points: 0,
             attack_description: nil,
---
+++ lib/dungeon_crawl/heroes.ex
@@ -0,0 +1,39 @@
+defmodule DungeonCrawl.Heroes do
+  alias DungeonCrawl.Character
+
+  def all,
+    do: [
+      %Character{
+        name: "Skywalker",
+        description: "Skywalker has strong defense and consistent damage.",
+        hit_points: 18,
+        max_hit_points: 18,
+        damage_range: 4..5,
+        attack_description: "a sword"
+      },
+      %Character{
+        name: "Darth",
+        description: "Darth has both high variability of attack damage and strong health.",
+        hit_points: 12,
+        max_hit_points: 12,
+        damage_range: 1..12,
+        attack_description: "a lightsaber"
+      },
+      %Character{
+        name: "Wizard",
+        description: "Wizard has strong attack, but low health.",
+        hit_points: 8,
+        max_hit_points: 8,
+        damage_range: 6..10,
+        attack_description: "a fireball"
+      },
+      %Character{
+        name: "Yoda",
+        description: "Rogue has high variability of attack damage.",
+        hit_points: 12,
+        max_hit_points: 20,
+        damage_range: 1..12,
+        attack_description: "a dagger"
+      }
+    ]
+end
