2bfa95868709ea2b4e67a20d292a2d034235605b
Rowa <jrowah@gmail.com>
2023-03-13 13:03:39 +0300

    includes mix in dialyzer analysis

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule DungeonCrawl.MixProject do
       version: "0.1.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      dialyzer: [plt_add_apps: [:mix]]
     ]
   end
 
@@ -21,8 +22,8 @@ defmodule DungeonCrawl.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      # dialyxir library, the version must be >= 0.5.0 and < 1.0.0, and is only needed in the dev environment
+      {:dialyxir, "~> 0.5", only: [:dev], runtime: false}
     ]
   end
 end
