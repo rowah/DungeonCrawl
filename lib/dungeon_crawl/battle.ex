d8b27a163abed4ee46aa5988e01c1f643d4a9fe3
Rowa <jrowah@gmail.com>
2023-03-28 09:42:32 +0300

    implements counter attachs

--- lib/dungeon_crawl/battle.ex
+++ lib/dungeon_crawl/battle.ex
@@ -11,8 +11,8 @@ defmodule DungeonCrawl.Battle do
       do: {char_a, char_b}
 
   def fight(char_a, char_b) do
-    char_a_after_damage = attack(char_a, char_b)
-    char_b_after_damage = attack(char_a_after_damage, char_b)
+    char_b_after_damage = attack(char_a, char_b)
+    char_a_after_damage = reverse_attack(char_a, char_b)
     fight(char_a_after_damage, char_b_after_damage)
   end
 
@@ -21,6 +21,7 @@ defmodule DungeonCrawl.Battle do
 
   # otherwise the attacked character receives a random amount of damage from the attacker’s damage range.
   def attack(char_a, char_b) do
+    # gives a random damage within the attacker's damage range
     damage = Enum.random(char_a.damage_range)
     char_b_after_damage = Character.take_damage(char_b, damage)
 
@@ -35,20 +36,37 @@ defmodule DungeonCrawl.Battle do
     char_b_after_damage
   end
 
+  def reverse_attack(char_a, char_b) do
+    # gives a random damage within the attacker's damage range
+    damage = Enum.random(char_b.damage_range)
+    char_a_after_damage = Character.take_damage(char_a, damage)
+
+    char_b
+    |> attack_message(damage)
+    |> Shell.info()
+
+    char_a_after_damage
+    |> receive_message(damage)
+    |> Shell.info()
+
+    char_a_after_damage
+  end
+
   defp attack_message(character = %{name: "You"}, damage) do
-    "You attack with #{character.attack_description} " <> "and deal #{damage} damage."
+    "You attack with #{character.attack_description} " <> "and deal #{damage} damages."
   end
 
   defp attack_message(character, damage) do
     "#{character.name} attacks with " <>
-      "#{character.attack_description} and " <> "deals #{damage} damage."
+      "#{character.attack_description} and " <> "deals #{damage} damages."
   end
 
   defp receive_message(character = %{name: "You"}, damage) do
-    "You receive #{damage}. Current HP: #{character.hit_points}."
+    "You receive #{damage} damages. Your current HP: #{character.hit_points}."
   end
 
   defp receive_message(character, damage) do
-    "#{character.name} receives #{damage}. " <> "Current HP: #{character.hit_points}."
+    "#{character.name} receives #{damage} damages. " <>
+      "#{character.name}'s current HP: #{character.hit_points}."
   end
 end
